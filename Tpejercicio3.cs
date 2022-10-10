6290dc96638ec39c0b633374e53a2d32fdf40dac
IsmaelMassolo <ismaelmassolo@gmail.com>
2022-10-10 18:17:23 -0300

    first commit

---
+++ Tpejercicio3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TpCSharpEjercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool logico = true;
+            int entero = 424;
+            string texto ="Esto es una cadena";
+            float conComa =143.26f;
+            Console.WriteLine("La variable del tipo lógico >> {0}", logico);
+            Console.WriteLine("La variable del tipo entero >> {0}", entero);
+            Console.WriteLine("La variable del tipo texto >> {0}", texto);
+            Console.WriteLine("La variable del tipo decimal >> {0}", conComa);
+        }
+    }
+}
